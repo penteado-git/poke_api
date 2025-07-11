ff59b602c1f2da47eafcf0a937fa495786fad1ce
Gabriel <fakcbos@gmail.com>
2025-07-11 16:55:22 -0300

    models teams

--- src/modules/teams/teams.module.ts
+++ src/modules/teams/teams.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from '../../trainers/team.entity';
+import { TeamController } from '../../controllers/team.controller';
+import { TeamService } from '../../services/team.service';
+import { TrainersModule } from '../trainers/trainers.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Team]),
+    TrainersModule,
   ],
-  controllers: [],
-  providers: [],
-  exports: [TypeOrmModule],
+  controllers: [TeamController],
+  providers: [TeamService],
+  exports: [TypeOrmModule, TeamService],
 })
 export class TeamsModule {}
