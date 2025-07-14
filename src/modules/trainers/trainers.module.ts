861e128caf098b7ef273f3e45a05be44e2c4b21d
Gabriel <fakcbos@gmail.com>
2025-07-14 00:45:11 -0300

    nem route for trainers module

--- src/modules/trainers/trainers.module.ts
+++ src/modules/trainers/trainers.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Trainer } from '../../trainers/trainer.entity';
 import { Team } from '../../trainers/team.entity';
 import { TeamPokemon } from '../../trainers/team-pokemon.entity';
+import { TrainerController } from 'src/controllers/trainer.controller';
+import { TrainerService } from '../../services/trainer.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Trainer, Team, TeamPokemon]),
   ],
-  controllers: [],
-  providers: [],
-  exports: [TypeOrmModule],
+  controllers: [TrainerController],
+  providers: [TrainerService],
+  exports: [TypeOrmModule, TrainerService],
 })
 export class TrainersModule {}
